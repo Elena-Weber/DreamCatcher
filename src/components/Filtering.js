7b46cfc7999aafb4fe0a7825d84d9e1801c69828
Elena-Weber <crochetmiracles@gmail.com>
2021-07-20 19:55:46 -0400

    turn radios into buttons

--- src/components/Filtering.js
+++ src/components/Filtering.js
@@ -4,12 +4,16 @@ const Filtering = (props) => {
 console.log(props)
     return (
         <div>Sort dreams
-        {/* <button value="Alphabetically" onClick={sortHandler}>alphabetically</button> */}
-        <label>
+        <button type="button" value="Alphabetically" name="sort" onClick={(e) => props.updateSort(e.target.value)}>alphabetically</button>
+        <button type="button" value="ReverseAlphabetically" name="sort" onClick={(e) => props.updateSort(e.target.value)}>ReverseAlphabetically</button>
+        <button type="button" value="CreationOld" name="sort" onClick={(e) => props.updateSort(e.target.value)}>Creation(oldest-youngest)</button>
+        <button type="button" value="CreationYoung" name="sort" onClick={(e) => props.updateSort(e.target.value)}>Creation(youngest-oldest)</button>
+
+        {/* <label>
         <input type="radio" value="Alphabetically" name="sort" checked={props.sort === 'Alphabetically'} onChange={(e) => props.updateSort(e.target.value)}/>
         Alphabetically
-        </label>
-        <label>
+        </label> */}
+        {/* <label>
         <input type="radio" value="ReverseAlphabetically" name="sort" checked={props.sort === 'ReverseAlphabetically'} onChange={(e) => props.updateSort(e.target.value)}/>
         ReverseAlphabetically
         </label>
@@ -20,7 +24,7 @@ console.log(props)
         <label>
         <input type="radio" value="CreationYoung" name="sort" checked={props.sort === 'CreationYoung'} onChange={(e) => props.updateSort(e.target.value)}/>
         Creation(youngest-oldest)
-        </label>
+        </label> */}
         </div>
     )
 }
--- src/containers/Dreams.js
+++ src/containers/Dreams.js
@@ -35,7 +35,6 @@ class Dreams extends Component {
         let lowerStateTerm = this.state.term.toLowerCase()
         let filteredDreams = this.props.dreams.filter(dream => dream.name.toLowerCase().includes(lowerStateTerm)
         )
-        // let reversedDreams = filteredDreams.reverse()
         console.log(filteredDreams)
 
         switch(this.state.sort){
@@ -50,34 +49,14 @@ class Dreams extends Component {
             default:
                 return filteredDreams.map( (dream, index) => { return <Dream dream={dream} key={index} /> })
         }
-
-        // return (reversedDreams.map( (dream, index) => { return <Dream dream={dream} key={index} /> }))
-        // return (filteredDreams.map( (dream, index) => { return <Dream dream={dream} key={index} /> }))
     } 
 
     updateSort = sortBy => {
-        console.log("HELLO FROM DREAMS")
         this.setState({ sort: sortBy })
-        console.log(this.state)
     }
-    // displayDreams = () => {
-    //     let sortedDreams = [...this.state]
-    //     switch(this.state.sort){
-    //         case "Alphabetically":
-    //             return sortedDreams.sort((a,b) => a.name > b.name ? 1 : -1)
-    //         case "Price":
-    //             return sortedDreams.sort((a,b) => a.price > b.price ? 1 : -1)
-    //         default:
-    //             return sortedDreams
-    //     }
-    // }
 
     render() { console.log(this.state.term)
 
-    // const dreams = this.props.dreams.map(( dream, index ) => <Dream key={ index } dream={ dream } />)
-    // let reverseDreams = dreams.reverse()
-
-//{debugger}
         return ( 
             <>
                 <div className="search">
@@ -88,9 +67,7 @@ class Dreams extends Component {
                 </div>
                 <div>
                 <h2 className="dream-heading">All your custom-made dreams are here. Enjoy!</h2>
-                {/* <div className="dreams">{ dreams }</div> */}
                 <div className="dreams">{this.filterIt()}</div>
-                {/* <div className="dreams">{ reverseDreams }</div> */}
                 </div>
             </>
         )
