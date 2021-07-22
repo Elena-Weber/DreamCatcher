75572525c718ade5d6cd702041577cb71239823c
Elena-Weber <crochetmiracles@gmail.com>
2021-07-22 15:41:46 -0400

    exchanged update versions places

--- src/reducers/dreamsReducer.js
+++ src/reducers/dreamsReducer.js
@@ -9,16 +9,16 @@ const dreamsReducer = (state = [], action) => {
             let newDreams = state.filter(dream => dream.id !== action.payload)
             return [...newDreams]
         case 'UPDATE_DREAM':
-            return state.map(dream => {
-                if( dream.id === action.payload.id) {
-                    return action.payload
-                } else {
-                    return dream
-                }
-            })
+            let dreamsAll = state.filter(dream => dream.id !== action.payload)
+            return [...dreamsAll, action.payload]
             // OR THIS ALSO WORKS
-            //let dreamsAll = state.filter(dream => dream.id !== action.payload)
-            //return [...dreamsAll, action.payload]
+            // return state.map(dream => {
+            //     if( dream.id === action.payload.id) {
+            //         return action.payload
+            //     } else {
+            //         return dream
+            //     }
+            // })
         default:
             return state
     }
